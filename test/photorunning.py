fd64e495ab1bccc888e01dc3ca0831e1fbba836e
Koji1116 <58416025+Koji1116@users.noreply.github.com>
2021-07-14 15:16:28 +0900

    Update photorunning.py

--- test/photorunning.py
+++ test/photorunning.py
@@ -66,7 +66,6 @@ def GoalDetection(imgpath, H_min=200, H_max=20, S_thd=80, G_thd=7000):
     		GAP = x+w/2-160
     		return [1, max_area, GAP, imgname]
     except:
-    	i = i + 1
     	return[i, -1, -1, imgname]
 
 if __name__ == "__main__":
