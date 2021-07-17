726f8da051d176eef60ab139e3f8a07d1743ae6d
Koji1116 <58416025+Koji1116@users.noreply.github.com>
2021-07-17 20:24:41 +0900

    Update panorama.py

--- Other/panorama.py
+++ Other/panorama.py
@@ -3,13 +3,14 @@ import os
 import glob
 import time
 
-path = '/home/pi/Desktop/Cansat2021ver/photosotorage/panorama'
+path = '/home/pi/Desktop/photosotorage/panorama'
 
 def panorama(srcdir,prefix='',srcext='.jpg',dstext='.jpg'):
     """
     パノラマを合成するための関数
     ソースディレクトリ内に合成用の写真を番号をつけて入れておく。（例：IMG0.jpg,IMG1.jpg）
     srcdir:ソースディレクトリ
+    dstdir:宛先ディレクトリ
     prefix:番号の前につける文字
     srcext:ソースの拡張子
     dstext:できたものの拡張子
@@ -18,7 +19,7 @@ def panorama(srcdir,prefix='',srcext='.jpg',dstext='.jpg'):
     resultcount = len(glob.glob1('result/', srcdir, '*'+dstext))
     photos = []
 
-    for i in range(0, srcfilecount):
+    for i in range(0, srcfilecount-1):
         photos.append(cv2.imread(srcdir +'/' + prefix + str(i) + srcext))
 
     stitcher = cv2.Stitcher.create(0)
