680528352f3ca10f623ac59dbd62f4d5c1d0f220
Koji1116 <58416025+Koji1116@users.noreply.github.com>
2021-07-19 14:54:23 +0900

    Update GPS_static.py

--- SensorModule/GPS/test/GPS_static.py
+++ SensorModule/GPS/test/GPS_static.py
@@ -13,8 +13,8 @@ RX = 20
 pi = pigpio.pi()
 
 #-------for test --------# takayama
-log_static_path = '/home/pi/Desktop/Cansat2021ver/SensorModule/GPS/test/static_test-'
-filecount = len(glob.glob1(log_static_path, '*' + '.txt'))
+log_static_path = '/home/pi/Desktop/Cansat2021ver/SensorModule/GPS/test'
+filecount = len(glob.glob1(log_static_path, 'GPS_static_test' + '*'))
 
 ELLIPSOID_GRS80 = 1  # GRS80
 ELLIPSOID_WGS84 = 2  # WGS84
