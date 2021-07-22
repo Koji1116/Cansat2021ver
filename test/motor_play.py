687fb302cc176786b0de771786bcaf0e9113a69b
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2021-07-22 16:47:09 +0900

    motor　コントローラファイル作ったよ

---
+++ test/motor_play.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.append('/home/pi/desktop/Cansat2021ver/SensorModule/6-axis')
+sys.path.append('/home/pi/desktop/Cansat2021ver/SensorModule/Motor')
+sys.path.append('/home/pi/desktop/Cansat2021ver/SensorModule/Communication')
+sys.path.append('/home/pi/desktop/Cansat2021ver/SensorModule/Motor')
+from time import sleep
+
+import motor
+
+while 1:
+    a =input()
+    if a =='a':
+        motor.motor(0.5,0.8,2)
+    elif a =='w':
+        motor.motor(0.8,0.8,2)
+    elif a =='d':
+        motor.motor(0.5,0.8,2)
+    elif a =='s':
+        motor.motor(-0.5,-0.5,2)
+    else:
+        pass
+
+    
