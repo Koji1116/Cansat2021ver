82985dd7cbeed290aedfd7aa8f44b698fd677db3
Koji1116 <58416025+Koji1116@users.noreply.github.com>
2021-07-23 11:03:20 +0900

    Update imageInfomation.py

--- test/imageInfomation.py
+++ test/imageInfomation.py
@@ -108,6 +108,7 @@ if __name__ == '__main__':
     try:
         while 1:
             G_thd = float(input('ゴールの閾値を入力，初期値:7000\t'))
+            time.sleep(1)
             photoName = Capture.Capture('photostorage/information', 320, 240)
             goalflug, goalarea, gap, _ = GoalDetection(photoName, G_thd)
             print(f'goalflug:{goalflug} goalarea{goalarea} gap{gap}')
