99418608a36d6671f7b67cf0d90923a8be82c0c6
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2021-07-23 11:47:07 +0900

    関数名変えたよ

--- SensorModule/Motor/motor.py
+++ SensorModule/Motor/motor.py
@@ -29,7 +29,7 @@ def motor_stop(x=1):
     time.sleep(x)
 
 
-def motor_move(strength_l, strength_r, time):
+def motor_move(strength_l, strength_r, time_wait):
     '''
     引数は左のmotorの強さ、右のmotorの強さ、走る時間。
     strength_l、strength_rは-1~1で表す。負の値だったら後ろ走行。
@@ -45,32 +45,32 @@ def motor_move(strength_l, strength_r, time):
         motor_l = Motor(Lpin1, Lpin2)
         motor_r.forward(strength_r)
         motor_l.forward(strength_l)
-        sleep(time)
+        sleep(time_wait)
     # 後進
     elif strength_r < 0 and strength_l < 0:
         motor_r = Motor(Rpin1, Rpin2)
         motor_l = Motor(Lpin1, Lpin2)
         motor_r.backward(abs(strength_r))
         motor_l.backward(abs(strength_l))
-        sleep(time)
+        sleep(time_wait)
     # 右回転
     elif strength_r >= 0 and strength_l < 0:
         motor_r = Motor(Rpin1, Rpin2)
         motor_l = Motor(Lpin1, Lpin2)
         motor_r.forkward(abs(strength_r))
         motor_l.backward(abs(strength_l))
-        sleep(time)
+        sleep(time_wait)
     # 左回転
     elif strength_r < 0 and strength_l >= 0:
         motor_r = Motor(Rpin1, Rpin2)
         motor_l = Motor(Lpin1, Lpin2)
         motor_r.backward(abs(strength_r))
         motor_l.forkward(abs(strength_l))
-        sleep(time)
+        sleep(time_wait)
 
 
-def motor(strength_l, strength_r, time, x=1):
-    motor_move(strength_l, strength_r, time)
+def move(strength_l, strength_r, time_wait, x=1):
+    motor_move(strength_l, strength_r, time_wait)
     motor_stop(x)
 
 #motor(0.8,0.8,3)
