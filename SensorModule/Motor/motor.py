dbaa476c1c751525e763defddb40398c164a9f42
Koji1116 <58416025+Koji1116@users.noreply.github.com>
2021-07-25 00:56:35 +0900

    Update motor.py

--- SensorModule/Motor/motor.py
+++ SensorModule/Motor/motor.py
@@ -8,20 +8,15 @@ import time
 
 
 
-# ピン番号は仮
-Rpin1 = 5
-Rpin2 = 6
 
-Lpin1 = 9
-Lpin2 = 10
 
 
 def motor_stop(x=1):
     '''motor_move()とセットで使用'''
-    Rpin1 = 5
-    Rpin2 = 6
-    Lpin1 = 9
-    Lpin2 = 10
+    Rpin1 = 9
+    Rpin2 = 10
+    Lpin1 = 5
+    Lpin2 = 6
     motor_r = Motor(Rpin1, Rpin2)
     motor_l = Motor(Lpin1, Lpin2)
     motor_r.stop()
@@ -35,10 +30,10 @@ def motor_move(strength_l, strength_r, t_moving):
     strength_l、strength_rは-1~1で表す。負の値だったら後ろ走行。
     必ずmotor_stop()セットで用いる。めんどくさかったら下にあるmotor()を使用
     '''
-    Rpin1 = 5
-    Rpin2 = 6
-    Lpin1 = 9
-    Lpin2 = 10
+    Rpin1 = 9
+    Rpin2 = 10
+    Lpin1 = 5
+    Lpin2 = 6
     # 前進するときのみスタック判定
     if strength_r >= 0 and strength_l >= 0:
         motor_r = Motor(Rpin1, Rpin2)
@@ -78,13 +73,24 @@ def motor(strength_l, strength_r, t_moving, x=0.1):
     strength_l /= 100
     motor_move(strength_l, strength_r, t_moving)
     t_stop = time.time()
-    if abs(strength_l) == abs(strength_r):
+    if abs(strength_l) == abs(strength_r) and strength_l * strength_r < 0:
         motor_stop(x)
     else:
-        while time.time() - t_stop <= 1:
-            coefficient_power = abs(1 - (time.time() - t_stop))
-            motor_move(strength_l*coefficient_power, strength_r*coefficient_power, 0.1)
-    motor_stop(x)
+        #before
+        # while time.time() - t_stop <= 1:
+        #     coefficient_power = abs(1 - (time.time() - t_stop))
+        #     motor_move(strength_l*coefficient_power, strength_r*coefficient_power, 0.1)
+
+
+        #更新(2021-07-24)
+        for i in range(10):
+            coefficient_power = 10 - i
+            coefficient_power /= 10
+            motor_move(strength_l * coefficient_power, strength_r * coefficient_power, 0.1)
+            if i == 9:
+                motor_stop(x)
+
+
 
 if __name__ == '__main__':
     while 1:
