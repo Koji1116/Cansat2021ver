c8e95df309acd3bbd04e8d60bab04a81f078ce77
Koji1116 <58416025+Koji1116@users.noreply.github.com>
2021-07-24 13:38:05 +0900

    Update panoramaShooingtest.py

--- test/panoramaShooingtest.py
+++ test/panoramaShooingtest.py
@@ -11,10 +11,76 @@ import panorama
 import Capture
 import BMC050
 import Xbee
-
-
-
+from gpiozero import Motor
 import Calibration
+
+Rpin1 = 5
+Rpin2 = 6
+
+Lpin1 = 9
+Lpin2 = 10
+
+def motor_stop(x=1):
+    '''motor_move()とセットで使用'''
+    motor_r = Motor(Rpin1, Rpin2)
+    motor_l = Motor(Lpin1, Lpin2)
+    motor_r.stop()
+    motor_l.stop()
+    time.sleep(x)
+
+
+def motor_move(strength_l, strength_r, t_moving):
+    '''
+    引数は左のmotorの強さ、右のmotorの強さ、走る時間。
+    strength_l、strength_rは-1~1で表す。負の値だったら後ろ走行。
+    必ずmotor_stop()セットで用いる。めんどくさかったら下にあるmotor()を使用
+    '''
+    # 前進するときのみスタック判定
+    if strength_r >= 0 and strength_l >= 0:
+        motor_r = Motor(Rpin1, Rpin2)
+        motor_l = Motor(Lpin1, Lpin2)
+        motor_r.forward(strength_r)
+        motor_l.forward(strength_l)
+        time.sleep(t_moving)
+    # 後進
+    elif strength_r < 0 and strength_l < 0:
+        motor_r = Motor(Rpin1, Rpin2)
+        motor_l = Motor(Lpin1, Lpin2)
+        motor_r.backward(abs(strength_r))
+        motor_l.backward(abs(strength_l))
+        time.sleep(t_moving)
+    # 右回転
+    elif strength_r >= 0 and strength_l < 0:
+        motor_r = Motor(Rpin1, Rpin2)
+        motor_l = Motor(Lpin1, Lpin2)
+        motor_r.forward(abs(strength_r))
+        motor_l.backward(abs(strength_l))
+        time.sleep(t_moving)
+    # 左回転
+    elif strength_r < 0 and strength_l >= 0:
+        motor_r = Motor(Rpin1, Rpin2)
+        motor_l = Motor(Lpin1, Lpin2)
+        motor_r.backward(abs(strength_r))
+        motor_l.forward(abs(strength_l))
+        time.sleep(t_moving)
+
+
+def motor_koji(strength_l, strength_r, t_moving, x=1):
+    """
+    急停止回避を組み込み 7/23 takayama
+    テストまだ
+    """
+    strength_r /= 100
+    strength_l /= 100
+    motor_move(strength_l, strength_r, t_moving)
+    t_stop = time.time()
+    if abs(strength_l) == abs(strength_r):
+        motor_stop(x)
+    else:
+        while time.time() - t_stop <= 1:
+            coefficient_power = abs(1 - (time.time() - t_stop))
+            motor_move(strength_l*coefficient_power, strength_r*coefficient_power, 0.1)
+    motor_stop(x)
 
 def panorama_shooting(l, r, t, magx_off, magy_off, path):
     """
@@ -35,7 +101,7 @@ def panorama_shooting(l, r, t, magx_off, magy_off, path):
         Capture.Capture(path)
         # filename = Capture.Capture(path)
         # photobox.append(filename)
-        motor.move(l, r, t)
+        motor_koji(l, r, t)
         magdata = BMC050.mag_dataRead()
         magx = magdata[0]
         magy = magdata[1]
@@ -45,7 +111,7 @@ def panorama_shooting(l, r, t, magx_off, magy_off, path):
         if latestθ - preθ <= 10:
             # Xbee.str_trans('Stuck')
             print('Stuck')
-            motor.move(1, 1, 3)
+            motor_koji(l, r, t)
             #----Initialize-----#
             magdata = BMC050.mag_dataRead()
             magx = magdata[0]
