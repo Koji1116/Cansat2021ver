2a9e2cb42d15577923428bb5da7d59ba9a005fb6
Koji1116 <58416025+Koji1116@users.noreply.github.com>
2021-07-24 11:22:49 +0900

    Update photorunning.py

--- test/photorunning.py
+++ test/photorunning.py
@@ -147,17 +147,17 @@ if __name__ == "__main__":
             if gap <= -30:
                 print('Turn left')
                 # Xbee.str_trans('Turn left')
-                motor_koji.motor_koji(0.2, -0.2, 0.3)
+                motor_koji.motor_koji(0.2, -0.2, 0.2)
                 # print('motor.motor(-0.2, 0.2, 0.3)')
                 # --- if the pixcel error is 30 or more, rotate right --- #
             elif 30 <= gap:
                 print('Turn right')
                 # Xbee.str_trans('Turn right')
-                motor_koji.motor_koji(-0.2, 0.2, 0.3)
+                motor_koji.motor_koji(-0.2, 0.2, 0.2)
                 # print('motor.motor(0.2, -0.2, 0.3)')
             elif gap == -1:
                 print('Nogoal detected')
-                motor_koji.motor_koji(-0.2, 0.2, 0.5)
+                motor_koji.motor_koji(-0.2, 0.2, 0.2)
                 # print('motor.motor(0.2, -0.2, 0.5)')
                 # --- if the pixcel error is greater than -30 and less than 30, go straight --- #
             else:
