431183f02be2a2925418f17da5114cae3c09c663
Koji1116 <58416025+Koji1116@users.noreply.github.com>
2021-07-24 14:40:39 +0900

    Update Calibration.py

--- Calibration/test/Calibration.py
+++ Calibration/test/Calibration.py
@@ -28,6 +28,7 @@ import GPS_Navigate
 import Other
 import glob
 from gpiozero import Motor
+import motor
 
 
 
@@ -39,71 +40,6 @@ RX = 18
 
 Calibration_rotate_controlLog = '/home/pi/log/Calibration_rotate_controlLog.txt'
 
-
-# ピン番号は仮
-Rpin1 = 5
-Rpin2 = 6
-
-Lpin1 = 9
-Lpin2 = 10
-
-
-def motor_stop(x=1):
-    '''motor_move()とセットで使用'''
-    Rpin1 = 5
-    Rpin2 = 6
-    Lpin1 = 9
-    Lpin2 = 10
-    motor_r = Motor(Rpin1, Rpin2)
-    motor_l = Motor(Lpin1, Lpin2)
-    motor_r.stop()
-    motor_l.stop()
-    time.sleep(x)
-
-
-def motor_move(strength_l, strength_r, t_wait):
-    '''
-    引数は左のmotorの強さ、右のmotorの強さ、走る時間。
-    strength_l、strength_rは-1~1で表す。負の値だったら後ろ走行。
-    必ずmotor_stop()セットで用いる。めんどくさかったら下にあるmotor()を使用
-    '''
-    Rpin1 = 5
-    Rpin2 = 6
-    Lpin1 = 9
-    Lpin2 = 10
-    # 前進するときのみスタック判定
-    if strength_r >= 0 and strength_l >= 0:
-        motor_r = Motor(Rpin1, Rpin2)
-        motor_l = Motor(Lpin1, Lpin2)
-        motor_r.forward(strength_r)
-        motor_l.forward(strength_l)
-        time.sleep(t_wait)
-    # 後進
-    elif strength_r < 0 and strength_l < 0:
-        motor_r = Motor(Rpin1, Rpin2)
-        motor_l = Motor(Lpin1, Lpin2)
-        motor_r.backward(abs(strength_r))
-        motor_l.backward(abs(strength_l))
-        time.sleep(t_wait)
-    # 右回転
-    elif strength_r >= 0 and strength_l < 0:
-        motor_r = Motor(Rpin1, Rpin2)
-        motor_l = Motor(Lpin1, Lpin2)
-        motor_r.forward(abs(strength_r))
-        motor_l.backward(abs(strength_l))
-        time.sleep(t_wait)
-    # 左回転
-    elif strength_r < 0 and strength_l >= 0:
-        motor_r = Motor(Rpin1, Rpin2)
-        motor_l = Motor(Lpin1, Lpin2)
-        motor_r.backward(abs(strength_r))
-        motor_l.forward(abs(strength_l))
-        time.sleep(t_wait)
-
-
-def motor(strength_l, strength_r, time, x=1):
-    motor_move(strength_l, strength_r, time)
-    motor_stop(x)
 
 def get_data():        
 	"""
@@ -325,4 +261,3 @@ if __name__ == "__main__":
 	
 	finally:
 		print("End")
-	
