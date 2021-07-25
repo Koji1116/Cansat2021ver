665279b6f4c6447d2ed9c1493fbeebee3ac65b69
Koji1116 <58416025+Koji1116@users.noreply.github.com>
2021-07-25 15:15:16 +0900

    Update runnningGPS_yoppi.py

--- test/runnningGPS_yoppi.py
+++ test/runnningGPS_yoppi.py
@@ -8,15 +8,15 @@ sys.path.append('/home/pi/Desktop/Cansat2021ver/SensorModule/GPS')
 sys.path.append('/home/pi/Desktop/Cansat2021ver/SensorModule/Motor')
 sys.path.append('/home/pi/Desktop/Cansat2021ver/Calibration')
 import numpy as np
-import gps_navigate
+import GPS_Navigate
 import Xbee
 import BMC050
 import GPS
 import stuck
 import motor
-import stuck
 import Calibration
 import pigpio
+from gpiozero import Motor
 import time
 import traceback
 from threading import Thread
