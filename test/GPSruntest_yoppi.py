0714b1c06aa0eec30aea9cf3f6ee4cc3d9be2410
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2021-07-25 15:01:39 +0900

    small

--- test/GPSruntest_yoppi.py
+++ test/GPSruntest_yoppi.py
@@ -5,21 +5,19 @@ sys.path.append('/home/pi/Desktop/Cansat2021ver/SensorModule/Camera')
 sys.path.append('/home/pi/Desktop/Cansat2021ver/SensorModule/GPS')
 sys.path.append('/home/pi/Desktop/Cansat2021ver/SensorModule/Motor')
 sys.path.append('/home/pi/Desktop/Cansat2021ver/Calibration')
-sys.path.append('/home/pi/Desktop/Cansat2021ver/SensorModule/6-axis')
-from gpiozero import Motor
 import numpy as np
-import mag
+import gps_navigate
+import Xbee
 import BMC050
-import GPS_Navigate
 import GPS
-import motor_koji
 import motor
-#import stuck
 import Calibration
 import pigpio
+from pigpio import Motor
 import time
 import traceback
 from threading import Thread
+import math
 
 
 # --- original module ---#
@@ -41,61 +39,62 @@ def timer(t):
 
 def adjust_direction(theta):
     """
-    
+    方向調整
     """
-    print('theta = '+str(theta)+'---rotate strat')
+    print('theta = '+str(theta)+'---回転調整開始！')
     count = 0
+    small = 20
+    big = 50
     while abs(theta) > 30:
         print(str(count))
-        if count > 8:
-            print('not ')
-            #stuck.stuck_avoid()
+        
+        #if count > 8:
+           # print('スタックもしくはこの場所が適切ではない')
+           # stuck.stuck_avoid()
 
         if abs(theta) <= 180:
             if abs(theta) <= 60:
-                print('theta = '+str(theta)+'---ver1')
-                motor.motor_move(
-                    np.sign(theta)*0.5, -1*np.sign(theta)*0.5, 0.1)
-                motor.stop()
-
+                
+                print('theta = '+str(theta)+'---回転開始ver1')
+                motor.move(np.cos(theta)*0.5 *100, -1*np.cos(theta)*0.5 *100, 0.1)
+            
             elif abs(theta) <= 180:
-                print('theta = '+str(theta)+'---ver2')
-                motor.motor_move(-np.sign(theta)
-                                 * 0.5, np.sign(theta)*0.5, 0.1)
-                motor.motor_stop()
+                
+                print('theta = '+str(theta)+'---回転開始ver2')
+                motor.move(-np.cos(theta) *0.5 *100, np.cos(theta)*0.5 *100, 0.2)
+                
         elif abs(theta) > 180:
             if abs(theta) >= 300:
-                print('theta = '+str(theta)+'---ver3')
-                motor.motor_move(-np.sign(theta)
-                                 * 0.5, np.sign(theta)*0.5, 0.1)
-                motor.motor_stop()
+                
+                print('theta = '+str(theta)+'---回転開始ver3')
+                motor.move(-np.cos(theta)*0.5 *100, np.cos(theta)*0.5 *100, 0.1)
+                
             elif abs(theta) > 180:
-                print('theta = '+str(theta)+'---ver4')
-                motor.motor_move(
-                    np.sign(theta)*0.5, -np.sign(theta)*0.5, 0.1)
-                motor.motor_stop()
+                
+                print('theta = '+str(theta)+'---回転開始ver4')
+                motor.move(np.cos(theta)*0.5 *100, -np.cos(theta)*0.5 *100, 0.2)
+               
         count += 1
         data = Calibration.get_data()
         magx = data[0]
         magy = data[1]
-        # --- 0 <=theta<= 360 ---#
-        theta = Calibration.calculate_angle_2D(
-            magx, magy, magx_off, magy_off)
+        # --- 0 <= θ <= 360 ---#
+        theta = Calibration.calculate_angle_2D(magx, magy, magx_off, magy_off)
         direction = Calibration.calculate_direction(lon2, lat2)
         azimuth = direction["azimuth1"]
         theta = theta-azimuth
 
-    print('theta = '+str(theta)+'---rotate fin!!!')
+    print('theta = '+str(theta)+'---回転終了!!!')
 
 
 if __name__ == "__main__":
-    mag.bmc050_setup()
+    BMC050.bmc050_setup()
     GPS.openGPS()
     print('Run Phase Start!')
-    print('GPSstart')
+    print('GPS走行開始')
     # --- difine goal latitude and longitude ---#
-    lon2 = 139.9082386
-    lat2 = 35.9184307
+    lon2 = 139.90833592590076
+    lat2 = 35.91817558805946
 
     # ------------- program start -------------#
     direction = Calibration.calculate_direction(lon2, lat2)
@@ -104,33 +103,38 @@ if __name__ == "__main__":
     # ------------- Calibration -------------#
     print('Calibration Start')
     # --- calculate offset ---#
-    mag.bmc050_setup()
-    ##-----------test--------
-    r = float(input('migi?'))
-    l = float(input('left?'))
-    t = float(input('rotate time?'))
+    BMC050.bmc050_setup()
+    ##-----------テスト用--------
+    r = float(input('右の出力は？'))
+    l = float(input('左の出力は？'))
+    t = float(input('一回の回転時間は？'))
     # --- calibration ---#
     magdata_Old = Calibration.magdata_matrix(l, r, t)
     magx_array_Old, magy_array_Old, magz_array_Old, magx_off, magy_off, magz_off = Calibration.calculate_offset(magdata_Old)
     time.sleep(0.1)
     print('GPS run strat')
     # ------------- GPS navigate -------------#
-    while 1:
+    while 1:  # この値調整必要
+
+        
 
         #----
         magdata = BMC050.mag_dataRead()
         mag_x = magdata[0]
         mag_y = magdata[1]
-        theta = Calibration.angle(mag_x, mag_y, magx_off, magy_off)
+        θ = Calibration.angle(mag_x, mag_y, magx_off, magy_off)
         print(mag_x,mag_y)
-        print(theta)
+        print(θ)
         time.sleep(0.5)
+        theta = θ
         adjust_direction(theta)
 
-        print('theta = '+str(theta)+'--strat straight')
-        motor_koji.motor_koji(80,80,10)
+        
+        print('theta = '+str(theta)+'---直進開始')
+        motor_koji.motor_koji(0.8,0.8,6)
 
         # --- calculate  goal direction ---#
         direction = Calibration.calculate_direction(lon2, lat2)
         goal_distance = direction["distance"]
         print(str(goal_distance))
+        
