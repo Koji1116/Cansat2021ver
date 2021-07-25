da3153a811a411d13dc5b61139999db3486f8bbc
Koji1116 <58416025+Koji1116@users.noreply.github.com>
2021-07-25 17:12:49 +0900

    Update photorunning.py

--- test/photorunning.py
+++ test/photorunning.py
@@ -205,16 +205,16 @@ if __name__ == "__main__":
             if -100 <= gap and gap <= -30:                                                                                  #調整するところ
                 print('Turn left')
                 # Xbee.str_trans('Turn left')
-                motor.move(-50, 50, 0.2)                                                       #調整するところ
+                motor.move(-50, 50, 0.1)                                                       #調整するところ
                 # print('motor.motor(-0.2, 0.2, 0.3)')
             elif 30 <= gap and gap <= 100:
                 print('Turn right')
                 # Xbee.str_trans('Turn right')
-                motor.move(50, -50, 0.2)                                                       #調整するところ
+                motor.move(50, -50, 0.1)                                                       #調整するところ
                 # print('motor.motor(0.2, -0.2, 0.3)')
             elif gap == 1000:
                 print('Nogoal detected')
-                motor.move(50, -50, 0.2)                                                       #調整するところ
+                motor.move(50, -50, 0.1)                                                       #調整するところ
                 # print('motor.motor(0.2, -0.2, 0.5)')
             else:
                 print('Go straight')
