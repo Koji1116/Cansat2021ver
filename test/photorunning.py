3138fb4b96b7090a795358817465139fbf88f391
Koji1116 <58416025+Koji1116@users.noreply.github.com>
2021-07-25 13:27:47 +0900

    Update photorunning.py

--- test/photorunning.py
+++ test/photorunning.py
@@ -198,7 +198,7 @@ if __name__ == "__main__":
         # photoName = 'photostorage/practice13.png'
         while goalflug != 0:
             photoName = Capture.Capture(path, 640, 320)                                               #解像度調整するところ？
-            goalflug, goalarea, gap, imgname = GoalDetection(photoName, G_thd)
+            goalflug, goalarea, gap, imgname = GoalDetection(photoName, 200, 20, 80, 50)
             print(f'goalflug:{goalflug}\tgoalarea:{goalarea}%\tgap:{gap}\timagename:{imgname}')
             time.sleep(1)
             # Xbee.str_trans('goalflug', goalflug, ' goalarea', goalarea, ' goalGAP', goalGAP)
