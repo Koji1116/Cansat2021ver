1f84e31e2626e7dbff44248b7ba80600ac0b41e3
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2021-07-26 14:10:31 +0900

    GPS読み込むまで撮り続ける関数導入してみた

--- Calibration/Calibration.py
+++ Calibration/Calibration.py
@@ -240,28 +240,33 @@ def calculate_angle_3D(accx, accy, accz, magx, magy, magz, magx_off, magy_off, m
 def calculate_direction(lon2, lat2):
     # --- read GPS data ---#
     try:
+        GPS.openGPS()
+        utc, lat, lon, sHeight, gHeight =GPS.GPSdata_read()
+        print(utc, lat, lon, sHeight, gHeight)
+        lat1 = lat
+        lon1 = lon
         
         #while True:
                     #GPS_data = GPS.readGPS()
                     #lat1 = GPS_data[1]
                     #lon1 = GPS_data[2]
-        while True:
-            utc, lat, lon, sHeight, gHeight = GPS.readGPS()
-            print(utc, lat, lon, sHeight, gHeight)
+        #while True:
+        #    utc, lat, lon, sHeight, gHeight = GPS.readGPS()
+        #    print(utc, lat, lon, sHeight, gHeight)#
 
-            if utc == -1.0:
-                if lat == -1.0:
-                    print("Reading GPS Error")
-                    # pass
-                else:
-                    # pass
-                    print("Status V")
-            else:
-                # pass
-                print(utc, lat, lon, sHeight, gHeight)
-                lat1 = lat
-                lon1 = lon
-                break
+        #    if utc == -1.0:
+        #        if lat == -1.0:
+        #            print("Reading GPS Error")
+        #           # pass
+        #        else:
+        #          # pass
+        #            print("Status V")
+        #    else:
+        #        # pass
+        #        print(utc, lat, lon, sHeight, gHeight)
+        #        lat1 = lat
+        #        lon1 = lon
+        #        break
         #while True:
           #  GPS_data = GPS.readGPS()
            # lat1 = GPS_data[1]
