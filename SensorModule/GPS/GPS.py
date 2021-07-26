349a65ef4c616bbb711fe62c29bc53dbd16d549d
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2021-07-26 14:05:02 +0900

    元に戻した

--- SensorModule/GPS/GPS.py
+++ SensorModule/GPS/GPS.py
@@ -266,30 +266,28 @@ def GPSdata_read():
         print("\r\nKeyboard Intruppted, Serial Closed")
 
 if __name__ == '__main__':
-    openGPS()
-    data = GPSdata_read()
-    print(data)
     
-    #try:
-    #    openGPS()
-    #    t_start = time.time()
-    #    while True:
-    #        utc, lat, lon, sHeight, gHeight = readGPS()
-    #        if utc == -1.0:
-    #            if lat == -1.0:
-    #                print("Reading GPS Error")
-    #                # pass
-    #            else:
-    #                # pass
-    #               print("Status V")
-    #        else:
-    #            # pass
-    #            print(utc, lat, lon, sHeight, gHeight)
-    #            Other.saveLog('GPStest', datetime.datetime.now(), time.time() - t_start, utc, lat, lon, sHeight, gHeight)
-    #        time.sleep(0.5)
-    #except KeyboardInterrupt:
-    #    closeGPS()
-    #    print("\r\nKeyboard Intruppted, Serial Closed")
-    #except:
-    #    closeGPS()
-    #    print(traceback.format_exc())
+    
+    try:
+        openGPS()
+        t_start = time.time()
+        while True:
+            utc, lat, lon, sHeight, gHeight = readGPS()
+            if utc == -1.0:
+                if lat == -1.0:
+                    print("Reading GPS Error")
+                    # pass
+                else:
+                    # pass
+                   print("Status V")
+            else:
+                # pass
+                print(utc, lat, lon, sHeight, gHeight)
+                Other.saveLog('GPStest', datetime.datetime.now(), time.time() - t_start, utc, lat, lon, sHeight, gHeight)
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        closeGPS()
+        print("\r\nKeyboard Intruppted, Serial Closed")
+    except:
+        closeGPS()
+        print(traceback.format_exc())
