8f8ece65b893ee467b88b99d9e1d174519a63d0e
oshima-yoppi <82073759+oshima-yoppi@users.noreply.github.com>
2021-07-27 19:00:35 +0900

    lon lat chabge

--- test/GPSruntest_yoppi.py
+++ test/GPSruntest_yoppi.py
@@ -99,8 +99,8 @@ if __name__ == "__main__":
     print('Run Phase Start!')
     print('GPS走行開始')
     # --- difine goal latitude and longitude ---#
-    lon2 = 139.9109691
-    lat2 = 35.9213278
+    lon2 = 139.9116062
+    lat2 = 35.9254365
 
     # ------------- program start -------------#
     direction = Calibration.calculate_direction(lon2, lat2)
