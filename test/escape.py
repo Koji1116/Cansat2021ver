85ea2e3a2d90938f6d6094e22bb63ff32e3347fe
Koji1116 <58416025+Koji1116@users.noreply.github.com>
2021-07-28 13:30:09 +0900

    Update escape.py

--- test/escape.py
+++ test/escape.py
@@ -10,7 +10,10 @@ def escape(t_melt):
         strength_l = i * 10
         strength_r = i * 10
         motor.move(strength_l, strength_r, 0.1)
-
+    for i in range(5):
+        strength_l = 50 - i * 10
+        strength_l = 50 - i * 10
+        motor.move(strength_l, strength_r, 0.1)
 
 if __name__ == '__main__':
     escape(t_melt=3)
