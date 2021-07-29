687c7a51c6f6bfa026b5ed6c3f7469394c889c75
Koji1116 <58416025+Koji1116@users.noreply.github.com>
2021-07-30 00:46:04 +0900

    Update release-land.py

--- test/release-land.py
+++ test/release-land.py
@@ -4,14 +4,43 @@ sys.path.append('/home/pi/Desktop/Cansat2021ver/SensorModule/Environmental')
 import BME280
 import time
 
+
+def pressdetect_release(anypress):
+    global presscount_release
+    global pressjudge_release
+    try:
+        pressdata = BME280.bme280_read()
+        prevpress = pressdata[1]
+        time.sleep(1)
+        pressdata = BME280.bme280_read()
+        latestpress = pressdata[1]
+        deltP = latestpress - prevpress
+        if 0.0 in pressdata:
+            print("BME280rror!")
+            pressjudge_release = 2
+            presscount_release = 0
+        elif deltP > anypress:
+            presscount_release += 1
+            if presscount_release > 2:
+                pressjudge_release = 1
+                print("pressreleasejudge")
+        else:
+            presscount_release = 0
+    except:
+        presscount_release = 0
+        pressjudge_release = 2
+    return presscount_release, pressjudge_release
+
+
 def pressdetect_land(anypress):
     """
     気圧情報による着地判定用
     引数はどのくらい気圧が変化したら判定にするかの閾値
     """
-    global Pcount
+    global Plandcount
     global pressdata
     presslandjudge = 0
+    plandcount = 0
     try:
         pressdata = BME280.bme280_read()
         Prevpress = pressdata[1]
@@ -22,50 +51,26 @@ def pressdetect_land(anypress):
         if 0.0 in pressdata:
             print("BME280error!")
             presslandjudge = 2
-            Pcount = 0
+            plandcount = 0
         elif deltP < anypress:
-            Pcount += 1
+            plandcount += 1
             if Pcount > 4:
                 presslandjudge = 1
                 print("presslandjudge")
         else:
-            Pcount = 0
+            plandcount = 0
     except:
-        print(traceback.format_exc())
         Pcount = 0
         presslandjudge = 2
-    return Plandcount, presslandjudge
+    return plandcount, presslandjudge
 
-def pressdetect_release(anypress):
 
-    pressreleasecount = 0
-    pressreleasejudge = 0
-    try:
-        pressdata = BME280.bme280_read()
-        prevpress = pressdata[1]
-        time.sleep(1)
-        pressdata = BME280.bme280_read()
-        latestpress = pressdata[1]
-        #print(presscount)
-        deltP = latestpress - prevpress
-        if 0.0 in pressdata:
-            print("BME280rror!")
-            pressreleasejudge = 2
-            pressreleasecount = 0
-        elif deltP > anypress:
-            pressreleasecount += 1
-            if pressreleasecount > 2:
-                pressreleasejudge = 1
-                print("pressreleasejudge")
-        else:
-            pressreleasecount = 0
-        #print(str(latestpress) + "	:	" + str(prevpress))
-    except:
-        pressreleasecount = 0
-        pressreleasejudge = 2
-    return pressreleasecount, pressreleasejudge
 
 if __name__ == '__main__':
-    bme280_setup()
-    bme280_calib_param()
+    BME280.bme280_setup()
+    BME280.bme280_calib_param()
     startTime = time.time()
+    presscount_release = 0
+    try:
+        while 1:
+            pressdetect_release(0.3)
