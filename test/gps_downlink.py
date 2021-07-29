3172c616b482306894b165b52e6d63c254b2c841
Koji1116 <58416025+Koji1116@users.noreply.github.com>
2021-07-29 13:29:44 +0900

    Create gps_downlink.py

---
+++ test/gps_downlink.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.append('/home/pi/Desktop/Cansat2021ver/SensorModule/Communication')
+sys.path.append('/home/pi/Desktop/Cansat2021ver/SensorModule/GPS')
+
+import GPS
+import Xbee
+
+
+if __name__ == '__main__':
+    try:
+        GPS.openGPS()
+        Xbee.on()
+        while 1:
+            _, lat, lon, _, _, = GPS.readGPS()
+            print(f'lat: {lat} \t lon:{lon}')
+            Xbee.str_trans(f'lat: {lat} \t lon:{lon}')
+    except KeyboardInterrupt:
+        print('Interrupted')
+        Xbee.str_trans('Interrupted')
+        GPS.closeGPS()
+        Xbee.off()
+
+
