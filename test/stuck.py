91881af61e6acb35dec47dd8dbcf8ca710207e17
yoppi <oosima@kimura-lab.net>
2021-08-01 10:16:57 +0900

    stuck 関数

--- test/stuck.py
+++ test/stuck.py
@@ -1,13 +1,16 @@
+import sys
+sys.path.append('/home/pi/desktop/Cansat2021ver/SensorModule/6-axis')
+sys.path.append('/home/pi/desktop/Cansat2021ver/SensorModule/Motor')
+sys.path.append('/home/pi/desktop/Cansat2021ver/SensorModule/Communication')
+sys.path.append('/home/pi/Desktop/Cansat2021ver/SensorModule/GPS')
 from SensorModule.Motor.stuck import stuck_jud
 from time import sleep
 from gpiozero import Motor
 import motor
 import BMC050
 import time
-import sys
-sys.path.append('/home/pi/desktop/Cansat2021ver/SensorModule/6-axis')
-sys.path.append('/home/pi/desktop/Cansat2021ver/SensorModule/Motor')
-sys.path.append('/home/pi/desktop/Cansat2021ver/SensorModule/Communication')
+import GPS_Navigate
+import GPS
 
 
 def stuck(thd=1):
@@ -35,36 +38,16 @@ def stuck(thd=1):
             return True
 
 
-def stuck_jud_test(thd=11):  # しきい値thd調整必要
-    BMC050.bmc050_setup()
-    global acc_max
-    for i in range(20):
-        accdata = BMC050.acc_data()
-        acc_x = accdata[0]
-        acc_y = accdata[1]
-        acc_z = accdata[2]
-        acc = (acc_x**2 + acc_y**2 + acc_z**2)**0.5
-        if acc_max < acc:
-            acc_max = acc
-
-    if acc_max < thd:
-        print('スタックした')
-        return True
-    else:
-        print('まだしてない')
-        return False
-
-
 if __name__ == '__main__':
-    acc_max = 0
-    while 1:
-        motor.motor(0.8, 0.8, 0.2)
-        if stuck_jud_test():
-            motor.stop()
-            print('fuck    acc_max ='+str(acc_max))
-            break
-        else:
-            print('not stucked   +acc_max='+str(acc_max))
-            time.sleep(5)
-            motor.stop()
-            time.sleep(2)
+    
+    
+    motor.motor(0.8, 0.8, 0.2)
+    if stuck_jud_test():
+        motor.stop()
+        print('fuck    acc_max ='+str(acc_max))
+        break
+    else:
+        print('not stucked   +acc_max='+str(acc_max))
+        time.sleep(5)
+        motor.stop()
+        time.sleep(2)
