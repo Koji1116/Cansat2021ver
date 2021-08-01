858ce61e5a597073e03b85a25fddc6d25acbcf74
yoppi <oosima@kimura-lab.net>
2021-08-01 10:03:31 +0900

    panorama1　も追加しといた

--- test/esc_para_gps_test.py
+++ test/esc_para_gps_test.py
@@ -27,6 +27,12 @@ import datetime
 # import goaldetection
 import Capture
 import photorunning
+import os
+import linecache
+import cv2
+import os
+import glob
+import time
 # 着地判定後にキャリブレーション前にパラシュート回避を行う
 # パラシュート回避エリアは設定せずに、パラシュートが検出されない場合が3回以上(下のプログラムのzで設定)
 # のときパラシュート回避を行ったと判定
@@ -155,6 +161,44 @@ def Parachute_Avoidance(flug, goalGAP):
         # print('flug')
         motor.move(70, 70, 1)
 
+def panorama(srcdir, dstdir, srcprefix='', dstprefix='',srcext='.jpg',dstext='.jpg'):
+    """
+    パノラマを合成するための関数
+    ソースディレクトリ内に合成用の写真を番号をつけて入れておく。（例：IMG0.jpg,IMG1.jpg）
+    宛先ディレクトリ内でソースディレクトリ＋番号の形でパノラマ写真が保存される。
+    撮影された写真次第ではパノラマ写真をできずエラーが出る可能性あるからtry,except必要？
+    srcdir:ソースディレクトリ
+    dstdir:宛先ディレクトリ
+    prefix:番号の前につける文字
+    srcext:ソースの拡張子
+    dstext:できたものの拡張子
+    """
+    srcfilecount = len(glob.glob1(srcdir + '/', '*'+srcext))
+    resultcount = len(glob.glob1(dstdir, srcdir + '*'+dstext))
+    print(srcfilecount)
+    print(resultcount)
+
+    photos = []
+
+    for i in range(0, srcfilecount):
+        if len(str(i)) == 1:
+            photos.append(cv2.imread(srcdir +'/' + srcprefix + '0' +  str(i) + srcext))
+        else:
+            photos.append(cv2.imread(srcdir + '/' + srcprefix + str(i) + srcext))
+
+
+
+
+    stitcher = cv2.Stitcher.create(0)
+    status, result = stitcher.stitch(photos)
+    cv2.imwrite(dstdir + '/' + dstprefix + str(resultcount) + srcext, result)
+
+    if status == 0:
+        print("success")
+    else:
+        print('failed')
+
+
 def adjust_direction(theta):
     global magx_off
     global magy_off
@@ -254,6 +298,20 @@ if __name__ == '__main__':
     except:
         print(traceback.format_exc())
     print("finish!")
+
+
+    print('panorama start')
+    try:
+        
+        srcdir = '/home/pi/Desktop/Cansat2021ver/test/nisho-ground12_640_asyuku'
+        dstdir = '/home/pi/Desktop/Cansat2021ver/test/photostorage'
+        startTime = time.time()  # プログラムの開始時刻
+        panorama(srcdir, dstdir, 'panoramaShootingtest00')
+        endTime = time.time() #プログラムの終了時間
+        runTime = endTime - startTime
+        print(runTime)
+    except KeyboardInterrupt:
+        print('Interrupted')
 
 
     mag.bmc050_setup()
