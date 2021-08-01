de7417bbfc3685c90c7c742a51b6a8ae17fba3b5
Koji1116 <58416025+Koji1116@users.noreply.github.com>
2021-08-01 12:19:48 +0900

    Update Xbee.py

--- SensorModule/Communication/Xbee.py
+++ SensorModule/Communication/Xbee.py
@@ -39,7 +39,7 @@ def str_trans(string):
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
         bytesize=serial.EIGHTBITS,
-        timeout=20
+        timeout=2
     )
     string = string + '\n'
     moji = string.encode()
