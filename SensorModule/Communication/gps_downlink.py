fdf9cdbf939a83823730108538cd0e471ff01a32
Koji1116 <58416025+Koji1116@users.noreply.github.com>
2021-08-03 11:41:34 +0900

    Update gps_downlink.py

--- SensorModule/Communication/gps_downlink.py
+++ SensorModule/Communication/gps_downlink.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
         GPS.openGPS()
         Xbee.on()
         while 1:
-            _, lat, lon, _, _, = GPS.readGPS()
+            _, lat, lon, _, _, = GPS.GPSdata_read()
             print(f'lat: {lat} \t lon:{lon}')
             Xbee.str_trans(f'lat: {lat} \t lon:{lon}\n \n')
     except KeyboardInterrupt:
