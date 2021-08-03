98e682c5ba75f1a7e8e2c9b9f021291a39a6e434
yoppi <oosima@kimura-lab.net>
2021-08-03 13:20:27 +0900

    errror

--- test/sensor.py
+++ test/sensor.py
@@ -5,6 +5,7 @@ sys.path.append('/home/pi/Desktop/Cansat2021ver/SensorModule/Environmental')
 sys.path.append('/home/pi/Desktop/Cansat2021ver/SensorModule/Illuminance')
 sys.path.append('/home/pi/Desktop/Cansat2021ver/SensorModule/Communication')
 sys.path.append('/home/pi/Desktop/Cansat2021ver/SensorModule/Motor')
+sys.path.append('/home/pi/Desktop/Cansat2021ver/SensorModule/Camera')
 sys.path.append('/home/pi/Desktop/Cansat2021ver/Other')
 import melt
 import BME280
