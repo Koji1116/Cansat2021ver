f56f120c2020eb7f393f9a56643e13927cd953a0
yoppi <oosima@kimura-lab.net>
2021-08-07 14:56:09 +0900

    pin

--- test/while_run_gps.py
+++ test/while_run_gps.py
@@ -28,10 +28,12 @@ import photorunning2
 import stuck
 import acc
 
-
+lon2 = 139.9114415
+lat2 = 35.9236391
 GPS.openGPS()
 acc.bmc050_setup()
 n = float(input('何秒間走る？'))
+
 while 1:
     print('----------------')
     old = acc.acc_dataRead()
@@ -40,12 +42,14 @@ while 1:
     new = acc.acc_dataRead()
     print(f'new {new}')
 
-
-
-    # GPSdata_old = GPS.GPSdata_read()
-    # motor.motor_move(30, 30, 5)
-    # GPSdata_new = GPS.GPSdata_read()
-    # if stuck.stuck_jug(GPSdata_old[1], GPSdata_old[2], GPSdata_new[1], GPSdata_new[2], 1.0):
-    #     pass
-    # else:
-    #     pass
+    ##---  方向合わせ----
+    direction = Calibration.calculate_direction(lon2, lat2)
+    goal_distance = direction['distance']
+    aaa = direction['azimuth1']
+    GPSdata_old = GPS.GPSdata_read()
+    motor.motor_move(30, 30, n)
+    GPSdata_new = GPS.GPSdata_read()
+    if stuck.stuck_jug(GPSdata_old[1], GPSdata_old[2], GPSdata_new[1], GPSdata_new[2], 1.0):
+        pass
+    else:
+        pass
