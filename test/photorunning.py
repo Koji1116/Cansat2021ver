055fd4cf711b57234cc7fc3395bd7cab661e63e6
Koji1116 <58416025+Koji1116@users.noreply.github.com>
2021-08-07 20:56:04 +0900

    画像誘導の変更

--- test/photorunning.py
+++ test/photorunning.py
@@ -1,14 +1,16 @@
 import sys
+
 sys.path.append('/home/pi/Desktop/Cansat2021ver/SensorModule/Camera')
 sys.path.append('/home/pi/Desktop/Cansat2021ver/SensorModule/Communication')
 sys.path.append('/home/pi/Desktop/Cansat2021ver/SensorModule/Motor')
 sys.path.append('/home/pi/Desktop/Cansat2021ver/Other')
-
+sys.path.append('/home/pi/Desktop/Cansat2021ver/Detection')
 sys.path.append('/home/pi/Desktop/Cansat2021ver/SensorModule/Communication')
 import cv2
 import numpy as np
 import time
 import Capture
+# import stuck
 import Xbee
 import motor
 import Other
@@ -16,10 +18,8 @@ import datetime
 from gpiozero import Motor
 import time
 
-
-
-#写真内の赤色面積で進時間を決める用　調整必要
-area_short = 20
+# 写真内の赤色面積で進時間を決める用　調整必要
+area_short = 59.9
 area_middle = 6
 area_long = 1
 
@@ -47,7 +47,7 @@ def GoalDetection(imgpath, H_min, H_max, S_thd, G_thd):
         img = cv2.imread(imgname)
         hig, wid, _ = img.shape
 
-        img_HSV = cv2.cvtColor(cv2.GaussianBlur(img,(15,15),0),cv2.COLOR_BGR2HSV_FULL)
+        img_HSV = cv2.cvtColor(cv2.GaussianBlur(img, (15, 15), 0), cv2.COLOR_BGR2HSV_FULL)
         h = img_HSV[:, :, 0]
         s = img_HSV[:, :, 1]
         mask = np.zeros(h.shape, dtype=np.uint8)
@@ -82,42 +82,100 @@ def GoalDetection(imgpath, H_min, H_max, S_thd, G_thd):
         return [1000, 1000, 1000, imgname]
 
 
+def DrawContours(imgpath, H_min, H_max, S_thd):
+    try:
+        img = cv2.imread(imgpath)
+        hig, wid, _ = img.shape
+
+        img_HSV = cv2.cvtColor(cv2.GaussianBlur(img, (15, 15), 0), cv2.COLOR_BGR2HSV_FULL)
+        h = img_HSV[:, :, 0]
+        s = img_HSV[:, :, 1]
+        mask = np.zeros(h.shape, dtype=np.uint8)
+        mask[((h < H_max) | (h > H_min)) & (s > S_thd)] = 255
+
+        contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        if len(contours) > 0:
+            for cnt in contours:
+                # 最小外接円を描く
+                (x, y), radius = cv2.minEnclosingCircle(cnt)
+                center = (int(x), int(y))
+                radius = int(radius)
+                radius_frame = cv2.circle(img, center, radius, (0, 0, 255), 2)
+
+            path = Other.fileName('photostorage/photorunning2-', 'jpg')
+            cv2.imwrite(path, radius_frame)
+                
+            return path
+        else:
+            path = Other.fileName('photostorage/photorunning2-', 'jpg')
+            cv2.imwrite(path, img)
+            return path
+    except KeyboardInterrupt:
+        print("end drawcircle")
+
+
 def image_guided_driving(path, G_thd):
+    goalflug = 1
     try:
         while goalflug != 0:
-            photoName = Capture.Capture(path, 320, 320)                                               #解像度調整するところ？
+            # if stuck.ue_jug() :
+            #     pass
+            # else:
+            #     motor.move(12, 12, 0.2)
+            photoName = Capture.Capture(path)  # 解像度調整するところ？
             goalflug, goalarea, gap, imgname = GoalDetection(photoName, 200, 20, 80, 50)
-            print(f'goalflug:{goalflug}\tgoalarea:{goalarea}%\tgap:{gap}\timagename:{imgname}')
-            # Xbee.str_trans('goalflug', goalflug, ' goalarea', goalarea, ' goalGAP', goalGAP)
+            imgname2 = DrawContours(imgname, 200, 20, 80)
+            print(f'goalflug:{goalflug}\tgoalarea:{goalarea}%\tgap:{gap}\timagename:{imgname}\timagename2:{imgname2}')
             # Other.saveLog(path,startTime - time.time(), goalflug, goalarea, goalGAP)
-            if -100 <= gap and gap <= -65:                                                                                  #調整するところ
-                print('Turn left')
-                # Xbee.str_trans('Turn left')
-                motor.move(-40, 40, 0.1)                                                       #調整するところ
-                # print('motor.motor(-0.2, 0.2, 0.3)')
-            elif 65 <= gap and gap <= 100:
-                print('Turn right')
-                # Xbee.str_trans('Turn right')
-                motor.move(40, -40, 0.1)                                                       #調整するところ
-                # print('motor.motor(0.2, -0.2, 0.3)')
-            elif gap == 1000 or gap==1000000:
+            if gap == 1000 or gap == 1000000:
                 print('Nogoal detected')
-                motor.move(40, -40, 0.1)                                                       #調整するところ
-                # print('motor.motor(0.2, -0.2, 0.5)')
-            else:
-                print('Go straight')
-                if goalarea <= area_long:
-                    motor.move(80, 80, 3)                                                        #調整するところ
-                    # print('motor.motor(1, 1, 10)')
-                    print('long')
-                elif goalarea <= area_middle:
-                    motor.move(80, 80, 1)                                                        #調整するところ
-                    # print('motor.motor(1, 1, 5)')
-                    print('middle')
-                elif goalarea <= area_short:
-                    motor.move(80, 80, 0.2)                                                      #調整するところ
-                    # print('motor.motor(1, 1, 2)')
-                    print('short')
+                motor.move(40, -40, 0.1)
+            elif goalarea <= area_long:
+                if -100 <= gap and gap <= -65:
+                    print('Turn left')
+                    # print('Turn left')
+                    motor.move(-40, 40, 0.1)
+                elif 65 <= gap and gap <= 100:
+                    print('Turn right')
+                    # print('Turn right')
+                    motor.move(40, -40, 0.1)
+                else:
+                    print('Go straight long')
+                    print('GO straight long')
+                    motor.move(80, 80, 3)
+            elif goalarea <= area_middle:
+                if -100 <= gap and gap <= -65:
+                    print('Turn left')
+                    # print('Turn left')
+                    motor.move(-25, 25, 0.1)
+                elif 65 <= gap and gap <= 100:
+                    print('Turn right')
+                    # print('Turn right')
+                    motor.move(25, -25, 0.1)
+                else:
+                    print('Go straight middle')
+                    # print('Go straight middle')
+                    motor.move(80, 80, 1)
+            elif goalarea <= area_short:
+                if -100 <= gap and gap <= -65:
+                    print('Turn left')
+                    # print('Turn left')
+                    motor.move(-15, 15, 0.1)
+                elif 65 <= gap and gap <= 100:
+                    print('Turn right')
+                    # print('Turn right')
+                    motor.move(15, -15, 0.1)
+                else:
+                    print('Go stright short')
+                    # print('Gos straight short')
+                    motor.move(40, 40, 0.2)
+            elif goalarea >= G_thd:
+                print('goal')
+                print('goal')
+                break
+        print('finish')
+        print('finish')
 
 
     except KeyboardInterrupt:
@@ -126,57 +184,59 @@ def image_guided_driving(path, G_thd):
         tb = sys.exc_info()[2]
         print("message:{0}".format(e.with_traceback(tb)))
 
+
 if __name__ == "__main__":
     try:
         # G_thd = float(input('ゴール閾値'))
         motor.setup()
-        G_thd = 80                                                                                          #調整するところ
+        G_thd = 80  # 調整するところ
         goalflug = 1
         startTime = time.time()
         dateTime = datetime.datetime.now()
-        path = f'photostorage/ImageGuidance_{dateTime.month}-{dateTime.day}-{dateTime.hour}:{dateTime.minute}'
-        # image_guided_driving(path, 50)
-
+        path = f'photostorage/ImageGuidance2_{dateTime.month}-{dateTime.day}-{dateTime.hour}-{dateTime.minute}'
+        image_guided_driving(path, G_thd)
 
         # photoName = 'photostorage/practice13.png'
-        while goalflug != 0:
-            photoName = Capture.Capture(path, 320, 320)                                               #解像度調整するところ？
-            goalflug, goalarea, gap, imgname = GoalDetection(photoName, 200, 20, 80, 50)
-            print(f'goalflug:{goalflug}\tgoalarea:{goalarea}%\tgap:{gap}\timagename:{imgname}')
-            # Xbee.str_trans('goalflug', goalflug, ' goalarea', goalarea, ' goalGAP', goalGAP)
-            # Other.saveLog(path,startTime - time.time(), goalflug, goalarea, goalGAP)
-            if -100 <= gap and gap <= -65:                                                                                  #調整するところ
-                print('Turn left')
-                # Xbee.str_trans('Turn left')
-                motor.move(-40, 40, 0.1)                                                       #調整するところ
-                # print('motor.motor(-0.2, 0.2, 0.3)')
-            elif 65 <= gap and gap <= 100:
-                print('Turn right')
-                # Xbee.str_trans('Turn right')
-                motor.move(40, -40, 0.1)                                                       #調整するところ
-                # print('motor.motor(0.2, -0.2, 0.3)')
-            elif gap == 1000 or gap==1000000:
-                print('Nogoal detected')
-                motor.move(40, -40, 0.1)                                                       #調整するところ
-                # print('motor.motor(0.2, -0.2, 0.5)')
-            else:
-                print('Go straight')
-                if goalarea <= area_long:
-                    motor.move(80, 80, 3)                                                        #調整するところ
-                    # print('motor.motor(1, 1, 10)')
-                    print('long')
-                elif goalarea <= area_middle:
-                    motor.move(80, 80, 1)                                                        #調整するところ
-                    # print('motor.motor(1, 1, 5)')
-                    print('middle')
-                elif goalarea <= area_short:
-                    motor.move(80, 80, 0.2)                                                      #調整するところ
-                    # print('motor.motor(1, 1, 2)')
-                    print('short')
+        # while goalflug != 0:
+        #     photoName = Capture.Capture(path, 320, 320)                                               #解像度調整するところ？
+        #     goalflug, goalarea, gap, imgname = GoalDetection(photoName, 200, 20, 80, 50)
+        #     print(f'goalflug:{goalflug}\tgoalarea:{goalarea}%\tgap:{gap}\timagename:{imgname}')
+        #     # print('goalflug', goalflug, ' goalarea', goalarea, ' goalGAP', goalGAP)
+        #     # Other.saveLog(path,startTime - time.time(), goalflug, goalarea, goalGAP)
+        #     if -100 <= gap and gap <= -65:                                                                                  #調整するところ
+        #         print('Turn left')
+        #         # print('Turn left')
+        #         motor.move(-40, 40, 0.1)                                                       #調整するところ
+        #         # print('motor.motor(-0.2, 0.2, 0.3)')
+        #     elif 65 <= gap and gap <= 100:
+        #         print('Turn right')
+        #         # print('Turn right')
+        #         motor.move(40, -40, 0.1)                                                       #調整するところ
+        #         # print('motor.motor(0.2, -0.2, 0.3)')
+        #     elif gap == 1000 or gap==1000000:
+        #         print('Nogoal detected')
+        #         motor.move(40, -40, 0.1)                                                       #調整するところ
+        #         # print('motor.motor(0.2, -0.2, 0.5)')
+        #     else:
+        #         print('Go straight')
+        #         if goalarea <= area_long:
+        #             motor.move(80, 80, 3)                                                        #調整するところ
+        #             # print('motor.motor(1, 1, 10)')
+        #             print('long')
+        #         elif goalarea <= area_middle:
+        #             motor.move(80, 80, 1)                                                        #調整するところ
+        #             # print('motor.motor(1, 1, 5)')
+        #             print('middle')
+        #         elif goalarea <= area_short:
+        #             motor.move(80, 80, 0.2)                                                      #調整するところ
+        #             # print('motor.motor(1, 1, 2)')
+        #             print('short')
 
 
     except KeyboardInterrupt:
         print('stop')
+        Xbee.off()
     except Exception as e:
+        Xbee.off()
         tb = sys.exc_info()[2]
         print("message:{0}".format(e.with_traceback(tb)))
