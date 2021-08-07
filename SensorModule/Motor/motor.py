86abb5ec4f3018fd32dc4b6082c688e74410e552
Koji1116 <58416025+Koji1116@users.noreply.github.com>
2021-08-07 20:20:32 +0900

    Update motor.py

--- SensorModule/Motor/motor.py
+++ SensorModule/Motor/motor.py
@@ -7,14 +7,23 @@ from time import sleep
 import time
 import acc
 
-def motor_stop(x=1):
-    '''motor_move()とセットで使用'''
-    Rpin1 = 6
-    Rpin2 = 5
-    Lpin1 = 9
-    Lpin2 = 10
+
+def setup_motor():
+    global motor_r, motor_l
+    Rpin1, Rpin2 = 6, 5
+    Lpin1, Lpin2 = 9, 10
     motor_r = Motor(Rpin1, Rpin2)
     motor_l = Motor(Lpin1, Lpin2)
+
+
+def motor_stop(x=1):
+    '''motor_move()とセットで使用'''
+    # Rpin1 = 6
+    # Rpin2 = 5
+    # Lpin1 = 9
+    # Lpin2 = 10
+    # motor_r = Motor(Rpin1, Rpin2)
+    # motor_l = Motor(Lpin1, Lpin2)
     motor_r.stop()
     motor_l.stop()
     time.sleep(x)
@@ -82,35 +91,23 @@ def motor_move(strength_l, strength_r, t_moving):
     '''
     strength_l = strength_l/100
     strength_r = strength_r/100
-    Rpin1 = 5
-    Rpin2 = 6
-    Lpin1 = 10
-    Lpin2 = 9
     # 前進するときのみスタック判定
     if strength_r >= 0 and strength_l >= 0:
-        motor_r = Motor(Rpin1, Rpin2)
-        motor_l = Motor(Lpin1, Lpin2)
         motor_r.forward(strength_r)
         motor_l.forward(strength_l)
         time.sleep(t_moving)
     # 後進
     elif strength_r < 0 and strength_l < 0:
-        motor_r = Motor(Rpin1, Rpin2)
-        motor_l = Motor(Lpin1, Lpin2)
         motor_r.backward(abs(strength_r))
         motor_l.backward(abs(strength_l))
         time.sleep(t_moving)
     # 右回転
     elif strength_r >= 0 and strength_l < 0:
-        motor_r = Motor(Rpin1, Rpin2)
-        motor_l = Motor(Lpin1, Lpin2)
         motor_r.forward(abs(strength_r))
         motor_l.backward(abs(strength_l))
         time.sleep(t_moving)
     # 左回転
     elif strength_r < 0 and strength_l >= 0:
-        motor_r = Motor(Rpin1, Rpin2)
-        motor_l = Motor(Lpin1, Lpin2)
         motor_r.backward(abs(strength_r))
         motor_l.forward(abs(strength_l))
         time.sleep(t_moving)
@@ -169,8 +166,7 @@ def move(strength_l, strength_r, t_moving, x=0.1):
 
 
 if __name__ == '__main__':
-    motor_r = Motor(5,6)
-    motor_l = Motor(10,9)
+    setup_motor()
     while 1:
         print(motor_r.is_active)
         print(motor_l.is_active)
