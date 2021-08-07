8711002fc0c68bef30baa8eb680ec3cb4b29a6b2
yoppi <oosima@kimura-lab.net>
2021-08-07 09:56:21 +0900

    ue

--- fall/falltest.py
+++ fall/falltest.py
@@ -29,6 +29,7 @@ import Calibration
 import release
 import land
 import motor
+import stuck
 
 pi = pigpio.pi()
 
@@ -184,7 +185,10 @@ if __name__ == '__main__':
         Xbee.str_trans('########-----Melted-----#######\n \n')
         # ------------------- ParaAvoidance Phase ------------------- #
         Xbee.str_trans("#####-----ParaAvo phase start-----#####")
-        motor.move(12,12,0.2)
+        if stuck.ue_jug():
+            pass
+        else:
+            motor.move(12,12,0.2)
 
 
         Other.saveLog(phaseLog, "6", "ParaAvoidance Phase Started", time.time() - t_start, datetime.datetime.now())
