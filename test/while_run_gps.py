f9b6e6c9a0a7afcf5e369ca0c36c590f6305f89d
yoppi <oosima@kimura-lab.net>
2021-08-08 23:12:05 +0900

    少し調整

--- test/while_run_gps.py
+++ test/while_run_gps.py
@@ -148,8 +148,8 @@ while 1:
         print(0)
         pass
     elif azimuth>15:
-        print(1)
-        run = 15
-    elif azimuth < 345:
-        run = -15
         print(-1)
+        run = -15
+    elif azimuth < 345:
+        run = 15
+        print(1)
