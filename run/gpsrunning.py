525ea65dbba9f7e7100d5c14c132193b75e39e1c
Koji1116 <58416025+Koji1116@users.noreply.github.com>
2021-08-09 17:37:03 +0900

    Update gpsrunning.py

--- run/gpsrunning.py
+++ run/gpsrunning.py
@@ -35,60 +35,6 @@ run_r = 0
 run = 0
 
 
-def adjust_direction(theta):
-    global magx_off
-    global magy_off
-    """
-    方向調整
-    """
-    print('ゴールとの角度theta = ' + str(theta) + '---回転調整開始！')
-
-    stuck_count = 1
-    t_small = 0.1
-    t_big = 0.2
-    force = 20
-    while 15 < theta < 345:
-        if stuck_count >= 7:
-            print('出力強くするよ')
-            force += 10
-        # if count > 8:
-        # print('スタックもしくはこの場所が適切ではない')
-        # stuck.stuck_avoid()
-
-        if theta <= 60:
-
-            print('theta = ' + str(theta) + '---回転開始ver1(' + str(stuck_count) + '回目)')
-            motor.move(force, -force, t_small)
-
-        elif 60 < theta <= 180:
-            print('theta = ' + str(theta) + '---回転開始ver2(' + str(stuck_count) + '回目)')
-            motor.move(force, -force, t_big)
-        elif theta >= 300:
-
-            print('theta = ' + str(theta) + '---回転開始ver3(' + str(stuck_count) + '回目)')
-            motor.move(-force, force, t_small)
-        elif 180 < theta < 360:
-
-            print('theta = ' + str(theta) + '---回転開始ver4(' + str(stuck_count) + '回目)')
-            motor.move(-force, force, t_big)
-
-        stuck_count += 1
-        data = Calibration.get_data()
-        magx = data[0]
-        magy = data[1]
-        # --- 0 <= θ <= 360 ---#
-        theta = Calibration.angle(magx, magy, magx_off, magy_off)
-        direction = Calibration.calculate_direction(lon2, lat2)
-        azimuth = direction["azimuth1"]
-        theta = azimuth - theta
-        if theta < 0:
-            theta = 360 + theta
-        elif 360 <= theta <= 450:
-            theta = theta - 360
-        print('計算後のゴールとなす角度theta' + str(theta))
-        time.sleep(1)
-
-    print('theta = ' + str(theta) + '---回転終了!!!')
 
 
 ##calibration
@@ -223,6 +169,64 @@ def drive(th_distance, t_adj_gps, logpath):
                 if i == 9:
                     motor.motor_stop(2)
 
+
+
+def adjust_direction(theta):
+    global magx_off
+    global magy_off
+    """
+    方向調整
+    """
+    print('ゴールとの角度theta = ' + str(theta) + '---回転調整開始！')
+
+    stuck_count = 1
+    t_small = 0.1
+    t_big = 0.2
+    force = 20
+    while 15 < theta < 345:
+        if stuck_count >= 7:
+            print('出力強くするよ')
+            force += 10
+        # if count > 8:
+        # print('スタックもしくはこの場所が適切ではない')
+        # stuck.stuck_avoid()
+
+        if theta <= 60:
+
+            print('theta = ' + str(theta) + '---回転開始ver1(' + str(stuck_count) + '回目)')
+            motor.move(force, -force, t_small)
+
+        elif 60 < theta <= 180:
+            print('theta = ' + str(theta) + '---回転開始ver2(' + str(stuck_count) + '回目)')
+            motor.move(force, -force, t_big)
+        elif theta >= 300:
+
+            print('theta = ' + str(theta) + '---回転開始ver3(' + str(stuck_count) + '回目)')
+            motor.move(-force, force, t_small)
+        elif 180 < theta < 360:
+
+            print('theta = ' + str(theta) + '---回転開始ver4(' + str(stuck_count) + '回目)')
+            motor.move(-force, force, t_big)
+
+        stuck_count += 1
+        data = Calibration.get_data()
+        magx = data[0]
+        magy = data[1]
+        # --- 0 <= θ <= 360 ---#
+        theta = Calibration.angle(magx, magy, magx_off, magy_off)
+        direction = Calibration.calculate_direction(lon2, lat2)
+        azimuth = direction["azimuth1"]
+        theta = azimuth - theta
+        if theta < 0:
+            theta = 360 + theta
+        elif 360 <= theta <= 450:
+            theta = theta - 360
+        print('計算後のゴールとなす角度theta' + str(theta))
+        time.sleep(1)
+
+    print('theta = ' + str(theta) + '---回転終了!!!')
+
+    
 if __name__ == '__main__':
     GPS.openGPS()
     acc.bmc050_setup()
