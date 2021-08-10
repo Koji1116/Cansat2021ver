edc4bae29186335d774ed01ec11cbbe4c050bf2d
Koji1116 <58416025+Koji1116@users.noreply.github.com>
2021-08-10 09:55:26 +0900

    Update gpsrunning_koji.py

--- run/gpsrunning_koji.py
+++ run/gpsrunning_koji.py
@@ -54,7 +54,7 @@ def adjust_direction(theta, magx_off, magy_off):
     t_small = 0.1
     t_big = 0.2
     force = 20
-    while 15 < theta <= 180 and -15 < theta < -180:
+    while 15 < theta <= 180 or -15 < theta < -180:
         if stuck_count >= 7:
             print('Increase output')
             force += 10
