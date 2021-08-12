36ae312e47077b1b66109fa158dd9637dcf2596c
yoppi <oosima@kimura-lab.net>
2021-08-12 14:05:53 +0900

    locate

--- run/gpsrunning_koji.py
+++ run/gpsrunning_koji.py
@@ -21,8 +21,8 @@ import stuck
 import acc
 import Xbee
 import Other
-lat2 = 35.868885
-lon2 = 139.924738
+lat2 = 35.8689285
+lon2 = 139.9244583
 
 
 def angle_goal(magx_off, magy_off):
@@ -148,8 +148,8 @@ def drive(lon2, lat2, thd_distance, t_adj_gps, logpath, t_start=0):
 
 
 if __name__ == '__main__':
-    lat2 = 35.868885
-    lon2 = 139.924738
+    lat2 = 35.8689285
+    lon2 = 139.9244583
     GPS.openGPS()
     acc.bmc050_setup()
     mag.bmc050_setup()
