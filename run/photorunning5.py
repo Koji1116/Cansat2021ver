d6ec5a824d6920bb942f9306febba5c6952f4a7c
Koji1116 <58416025+Koji1116@users.noreply.github.com>
2021-08-26 01:24:04 +0900

    整形

--- run/photorunning5.py
+++ run/photorunning5.py
@@ -223,14 +223,14 @@ def image_guided_driving(log_photorunning, G_thd, magx_off, magy_off, lon2, lat2
                     if count_short_r % 4 == 0:
                         adj_short += 3
                         print('#-Power up-#')
-                    motor.move(-20-adj_short, 20+adj_short, 0.1)
+                    motor.move(-20 - adj_short, 20 + adj_short, 0.1)
                 elif 65 <= gap and gap <= 100:
                     print('Turn right')
                     count_short_r += 1
                     if count_short_r % 4 == 0:
                         adj_short += 3
                         print('#-Power up-#')
-                    motor.move(20+adj_short, -20-adj_short, 0.1)
+                    motor.move(20 + adj_short, -20 - adj_short, 0.1)
                 else:
                     print('Go stright short')
                     adjustment_mag(40, 0.7, magx_off, magy_off)
