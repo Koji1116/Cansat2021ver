4308356b0dbb6b4c5dba3d775e1ef44d526e5a07
yoppi <oosima@kimura-lab.net>
2021-08-25 16:55:47 +0900

    stuck_avoid_move 変更

--- Detection/stuck.py
+++ Detection/stuck.py
@@ -74,10 +74,12 @@ def random(a, b, k):
 def stuck_avoid_move(x):
     if x == 0:
         print('sutck_avoid_move():0')
+        motor.moev(40, -40, 1)
         motor.move(100, 100, 5)
         motor.move(60, 60, 3)
     elif x == 1:
         print('sutck_avoid_move():1')
+        motor.moev(40, -40, 1)
         motor.move(-100, -100, 5)
         motor.move(-60, -60, 3)
     elif x == 2:
@@ -92,17 +94,19 @@ def stuck_avoid_move(x):
 
     elif x == 4:
         print('sutck_avoid_move():4')
+        motor.moev(40, -40, 1)
         motor.move(-80, -100, 5)
         motor.move(-60, -60, 3)
 
     elif x == 5:
         print('sutck_avoid_move():5')
+        motor.moev(40, -40, 1)
         motor.move(-100, -80, 5)
         motor.move(-60, -60, 3)
 
     elif x == 6:
         print('sutck_avoid_move():6')
-        motor.move(100, -100, 5)
+        motor.move(100, -100, 3)
         motor.move(100, 100, 3)
 
 
