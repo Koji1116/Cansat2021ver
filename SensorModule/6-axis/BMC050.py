507279c491e0d02feeb3d40a9ca0e3ec5dbedb48
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2021-08-26 15:38:36 +0900

    mag error 出てきても対応できるようにしました

--- SensorModule/6-axis/BMC050.py
+++ SensorModule/6-axis/BMC050.py
@@ -104,23 +104,29 @@ def mag_dataRead():
     # --- Read Mag Data --- #
     magData = [0, 0, 0, 0, 0, 0, 0, 0]
     value = [0.0, 0.0, 0.0]
-    for i in range(8):
-        try:
-            magData[i] = i2c.read_byte_data(
-                MAG_ADDRESS, MAG_REGISTER_ADDRESS + i)
-        except:
-            pass
-            # print("error")
+    while 1:
+        for i in range(8):
+            try:
+                magData[i] = i2c.read_byte_data(
+                    MAG_ADDRESS, MAG_REGISTER_ADDRESS + i)
+            except:
+                pass
+                # print("error")
 
-    for i in range(3):
-        if i != 2:
-            value[i] = ((magData[2*i+1] * 256) + (magData[2*i] & 0xF8)) / 8
-            if value[i] > 4095:
-                value[i] = value[i] - 8192
+        for i in range(3):
+            if i != 2:
+                value[i] = ((magData[2*i+1] * 256) + (magData[2*i] & 0xF8)) / 8
+                if value[i] > 4095:
+                    value[i] = value[i] - 8192
+            else:
+                value[i] = ((magData[2*i+1] * 256) | (magData[2*i] & 0xF8)) / 2
+                if value[i] > 16383:
+                    value[i] = value[i] - 32768
+        
+        if value == [0.0, 0.0, 0.0]:
+            BMC050_error()
         else:
-            value[i] = ((magData[2*i+1] * 256) | (magData[2*i] & 0xF8)) / 2
-            if value[i] > 16383:
-                value[i] = value[i] - 32768
+            break
 
     return value
 
@@ -144,6 +150,7 @@ def BMC050_error():
     """
     6軸センサエラー起きたらこの関数使ってね。
     """
+    print('------mag error------switch start')
     BMC050_off()
     time.sleep(0.1)
     BMC050_setup()
