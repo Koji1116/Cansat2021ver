2f81dc4c7060a1c4b8f32bd34cbfa77d210b6bbd
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2021-08-26 15:55:48 +0900

    locate

--- run/gpsrunning_koji.py
+++ run/gpsrunning_koji.py
@@ -217,8 +217,8 @@ def drive(lon2, lat2, thd_distance, t_adj_gps, logpath = '/home/pi/Desktop/Cansa
 
 
 if __name__ == '__main__':
-    lat2 = 35.868631
-    lon2 = 139.924717
+    lat2 = 35.9185205
+    lon2 = 139.9085615
     GPS.openGPS()
     acc.bmc050_setup()
     mag.bmc050_setup()
